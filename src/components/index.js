9e8155dab24ee6bfd2d15d2ccfbc46128692de4f
phpstorm <jc675@njit.edu>
2018-04-28 21:39:36 -0400

    Added To-Do File

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+
+
+import React, {Component} from 'react';
+import { View, Text } from "react-native";
+
+class Index extends Component {
+
+
+    render() {
+        return(
+            <Text>
+                Welcome to your personal ToDo-List
+            </Text>
+        )
+    }
+
+}
+
+export default Index;
